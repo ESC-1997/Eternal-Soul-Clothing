ccbc213d650736cbaf07da00b1eed25f5dd76795
ESC-1997 <info@eternalsoul.co>
2025-05-04 09:08:31 -0500

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -9,17 +9,44 @@ export default function Shop() {
 
   return (
     <main className="min-h-screen bg-[#DADBE4]">
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto relative">
         {!isCustomizing && !isViewing && (
-          <div className="flex justify-center py-8">
-            <Image
-              src="/images/Phoenix_ES_1B1F3B.png"
-              alt="Eternal Soul"
-              width={300}
-              height={100}
-              className="object-contain"
+          <>
+            <div className="flex justify-center py-8">
+              <Image
+                src="/images/Phoenix_ES_1B1F3B.png"
+                alt="Eternal Soul"
+                width={300}
+                height={100}
+                className="object-contain"
+              />
+            </div>
+            {/* Hardcoded bubble overlays */}
+            <img
+              src="/images/bubble.png"
+              alt="Customizable Overlay 1"
+              style={{
+                position: 'absolute',
+                left: '135px',
+                top: '330px',
+                width: '100px',
+                height: '90px',
+                zIndex: 30,
+              }}
             />
-          </div>
+            <img
+              src="/images/bubble.png"
+              alt="Customizable Overlay 2"
+              style={{
+                position: 'absolute',
+                left: '360px',
+                top: '330px',
+                width: '100px',
+                height: '90px',
+                zIndex: 30,
+              }}
+            />
+          </>
         )}
         <PrintifyStore 
           onCustomizationModeChange={setIsCustomizing} 
