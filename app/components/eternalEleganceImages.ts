b449ee551871b58bbd914dead6600ae6564aed55
ESC-1997 <info@eternalsoul.co>
2025-05-03 21:50:33 -0500

    Create eternalEleganceImages.ts

---
+++ app/components/eternalEleganceImages.ts
@@ -0,0 +1,161 @@
+// This file is auto-generated to map available Eternal Elegance images by shirt and logo color.
+// Only combinations that exist in the /public/images/eternal_elegance folder are included.
+
+export const eternalEleganceImages: {
+  [shirtColor: string]: {
+    [logoColor: string]: string[];
+  };
+} = {
+  Charcoal: {
+    red: [
+      '/images/eternal_elegance/elegance_charcoal_red.jpg',
+      '/images/eternal_elegance/elegance_charcoal_red1.jpg',
+    ],
+    blue: [
+      '/images/eternal_elegance/elegance_charcoal_blue.jpg',
+      '/images/eternal_elegance/elegance_charcoal_blue1.jpg',
+    ],
+    white: [
+      '/images/eternal_elegance/elegance_charcoal_white.jpg',
+      '/images/eternal_elegance/elegance_charcoal_white1.jpg',
+    ],
+    violet: [
+      '/images/eternal_elegance/elegance_charcoal_violet.jpg',
+      '/images/eternal_elegance/elegance_charcoal_violet1.jpg',
+    ],
+    grey: [
+      '/images/eternal_elegance/elegance_charcoal_grey.jpg',
+      '/images/eternal_elegance/elegance_charcoal_grey1.jpg',
+    ],
+    black: [
+      '/images/eternal_elegance/elegance_charcoal_black.jpg',
+      '/images/eternal_elegance/elegance_charcoal_black1.jpg',
+    ],
+  },
+  Black: {
+    red: [
+      '/images/eternal_elegance/elegance_black_red.jpg',
+      '/images/eternal_elegance/elegance_black_red1.jpg',
+    ],
+    blue: [
+      '/images/eternal_elegance/elegance_black_blue.jpg',
+      '/images/eternal_elegance/elegance_black_blue1.jpg',
+    ],
+    white: [
+      '/images/eternal_elegance/elegance_black_white.jpg',
+      '/images/eternal_elegance/elegance_black_white1.jpg',
+    ],
+    violet: [
+      '/images/eternal_elegance/elegance_black_violet.jpg',
+      '/images/eternal_elegance/elegance_black_violet1.jpg',
+    ],
+    grey: [
+      '/images/eternal_elegance/elegance_black_grey.jpg',
+      '/images/eternal_elegance/elegance_black_grey1.jpg',
+    ],
+  },
+  'Forest Green': {
+    red: [
+      '/images/eternal_elegance/elegance_fgreen_red.jpg',
+      '/images/eternal_elegance/elegance_fgreen_red1.jpg',
+    ],
+    blue: [
+      '/images/eternal_elegance/elegance_fgreen_blue.jpg',
+      '/images/eternal_elegance/elegance_fgreen_blue1.jpg',
+    ],
+    white: [
+      '/images/eternal_elegance/elegance_fgreen_white.jpg',
+      '/images/eternal_elegance/elegance_fgreen_white1.jpg',
+    ],
+    violet: [
+      '/images/eternal_elegance/elegance_fgreen_violet.jpg',
+      '/images/eternal_elegance/elegance_fgreen_violet1.jpg',
+    ],
+    grey: [
+      '/images/eternal_elegance/elegance_fgreen_grey.jpg',
+      '/images/eternal_elegance/elegance_fgreen_grey1.jpg',
+    ],
+    black: [
+      '/images/eternal_elegance/elegance_fgreen_black.jpg',
+      '/images/eternal_elegance/elegance_fgreen_black1.jpg',
+    ],
+  },
+  'Light Blue': {
+    red: [
+      '/images/eternal_elegance/elegance_lblue_red.jpg',
+      '/images/eternal_elegance/elegance_lblue_red1.jpg',
+    ],
+    blue: [
+      '/images/eternal_elegance/elegance_lblue_blue.jpg',
+      '/images/eternal_elegance/elegance_lblue_blue1.jpg',
+    ],
+    white: [
+      '/images/eternal_elegance/elegance_lblue_white.jpg',
+      '/images/eternal_elegance/elegance_lblue_white1.jpg',
+    ],
+    violet: [
+      '/images/eternal_elegance/elegance_lblue_violet.jpg',
+      '/images/eternal_elegance/elegance_lblue_violet1.jpg',
+    ],
+    grey: [
+      '/images/eternal_elegance/elegance_lblue_grey.jpg',
+      '/images/eternal_elegance/elegance_lblue_grey1.jpg',
+    ],
+    black: [
+      '/images/eternal_elegance/elegance_lblue_black.jpg',
+      '/images/eternal_elegance/elegance_lblue_black1.jpg',
+    ],
+  },
+  Sand: {
+    red: [
+      '/images/eternal_elegance/elegance_sand_red.jpg',
+      '/images/eternal_elegance/elegance_sand_red1.jpg',
+    ],
+    blue: [
+      '/images/eternal_elegance/elegance_sand_blue.jpg',
+      '/images/eternal_elegance/elegance_sand_blue1.jpg',
+    ],
+    white: [
+      '/images/eternal_elegance/elegance_sand_white.jpg',
+      '/images/eternal_elegance/elegance_sand_white1.jpg',
+    ],
+    violet: [
+      '/images/eternal_elegance/elegance_sand_violet.jpg',
+      '/images/eternal_elegance/elegance_sand_violet1.jpg',
+    ],
+    grey: [
+      '/images/eternal_elegance/elegance_sand_grey.jpg',
+      '/images/eternal_elegance/elegance_sand_grey1.jpg',
+    ],
+    black: [
+      '/images/eternal_elegance/elegance_sand_black.jpg',
+      '/images/eternal_elegance/elegance_sand_black1.jpg',
+    ],
+  },
+  White: {
+    red: [
+      '/images/eternal_elegance/elegance_white_red.jpg',
+      '/images/eternal_elegance/elegance_white_red1.jpg',
+    ],
+    blue: [
+      '/images/eternal_elegance/elegance_white_blue.jpg',
+      '/images/eternal_elegance/elegance_white_blue1.jpg',
+    ],
+    white: [
+      '/images/eternal_elegance/elegance_white_white.jpg',
+      '/images/eternal_elegance/elegance_white_white1.jpg',
+    ],
+    violet: [
+      '/images/eternal_elegance/elegance_white_violet.jpg',
+      '/images/eternal_elegance/elegance_white_violet1.jpg',
+    ],
+    grey: [
+      '/images/eternal_elegance/elegance_white_grey.jpg',
+      '/images/eternal_elegance/elegance_white_grey1.jpg',
+    ],
+    black: [
+      '/images/eternal_elegance/elegance_white_black.jpg',
+      '/images/eternal_elegance/elegance_white_black1.jpg',
+    ],
+  },
+}; 
