2ed327525edae869490a81b6e0c2b180d8203e38
ESC-1997 <info@eternalsoul.co>
2025-05-08 14:30:51 -0500

    Update eternalEleganceVariants.ts

--- app/components/eternalEleganceVariants.ts
+++ app/components/eternalEleganceVariants.ts
@@ -222,59 +222,59 @@ export const eternalEleganceVariants = {
   },
   "68163317960c7decc0099499": { // Grey logo (different prices)
     "Black": {
-      "XS": { variant_id: 67831, price: 1722, stock_status: "In Stock" },
-      "S": { variant_id: 38164, price: 1722, stock_status: "In Stock" },
-      "M": { variant_id: 38178, price: 1722, stock_status: "In Stock" },
-      "L": { variant_id: 38192, price: 1722, stock_status: "In Stock" },
-      "XL": { variant_id: 38206, price: 1722, stock_status: "In Stock" },
-      "2XL": { variant_id: 38220, price: 1952, stock_status: "In Stock" },
-      "3XL": { variant_id: 42122, price: 2145, stock_status: "In Stock" },
-      "4XL": { variant_id: 66213, price: 2367, stock_status: "In Stock" }
+      "XS": { variant_id: 67831, price: 3500, stock_status: "In Stock" },
+      "S": { variant_id: 38164, price: 3500, stock_status: "In Stock" },
+      "M": { variant_id: 38178, price: 3500, stock_status: "In Stock" },
+      "L": { variant_id: 38192, price: 3500, stock_status: "In Stock" },
+      "XL": { variant_id: 38206, price: 3500, stock_status: "In Stock" },
+      "2XL": { variant_id: 38220, price: 3500, stock_status: "In Stock" },
+      "3XL": { variant_id: 42122, price: 3750, stock_status: "In Stock" },
+      "4XL": { variant_id: 66213, price: 3750, stock_status: "In Stock" }
     },
     "Charcoal": {
-      "XS": { variant_id: 67802, price: 1722, stock_status: "In Stock" },
-      "S": { variant_id: 38153, price: 1722, stock_status: "In Stock" },
-      "M": { variant_id: 38167, price: 1722, stock_status: "In Stock" },
-      "L": { variant_id: 38181, price: 1722, stock_status: "In Stock" },
-      "XL": { variant_id: 38195, price: 1722, stock_status: "In Stock" },
-      "2XL": { variant_id: 38209, price: 1952, stock_status: "In Stock" },
-      "3XL": { variant_id: 42109, price: 2145, stock_status: "In Stock" },
-      "4XL": { variant_id: 66186, price: 2367, stock_status: "In Stock" }
+      "XS": { variant_id: 67802, price: 3500, stock_status: "In Stock" },
+      "S": { variant_id: 38153, price: 3500, stock_status: "In Stock" },
+      "M": { variant_id: 38167, price: 3500, stock_status: "In Stock" },
+      "L": { variant_id: 38181, price: 3500, stock_status: "In Stock" },
+      "XL": { variant_id: 38195, price: 3500, stock_status: "In Stock" },
+      "2XL": { variant_id: 38209, price: 3500, stock_status: "In Stock" },
+      "3XL": { variant_id: 42109, price: 3750, stock_status: "In Stock" },
+      "4XL": { variant_id: 66186, price: 3750, stock_status: "In Stock" }
     },
     "Forest Green": {
-      "S": { variant_id: 38165, price: 1722, stock_status: "In Stock" },
-      "M": { variant_id: 38179, price: 1722, stock_status: "In Stock" },
-      "L": { variant_id: 38193, price: 1722, stock_status: "In Stock" },
-      "XL": { variant_id: 38207, price: 1722, stock_status: "In Stock" },
-      "2XL": { variant_id: 38221, price: 1952, stock_status: "In Stock" },
-      "3XL": { variant_id: 42123, price: 2145, stock_status: "In Stock" }
+      "S": { variant_id: 38165, price: 3500, stock_status: "In Stock" },
+      "M": { variant_id: 38179, price: 3500, stock_status: "In Stock" },
+      "L": { variant_id: 38193, price: 3500, stock_status: "In Stock" },
+      "XL": { variant_id: 38207, price: 3500, stock_status: "In Stock" },
+      "2XL": { variant_id: 38221, price: 3500, stock_status: "In Stock" },
+      "3XL": { variant_id: 42123, price: 3750, stock_status: "In Stock" }
     },
     "Light Blue": {
-      "S": { variant_id: 38157, price: 1722, stock_status: "In Stock" },
-      "M": { variant_id: 38171, price: 1722, stock_status: "In Stock" },
-      "L": { variant_id: 38185, price: 1722, stock_status: "In Stock" },
-      "XL": { variant_id: 38199, price: 1722, stock_status: "In Stock" },
-      "2XL": { variant_id: 38213, price: 1952, stock_status: "In Stock" },
-      "3XL": { variant_id: 42114, price: 2145, stock_status: "In Stock" },
-      "4XL": { variant_id: 66200, price: 2367, stock_status: "In Stock" }
+      "S": { variant_id: 38157, price: 3500, stock_status: "In Stock" },
+      "M": { variant_id: 38171, price: 3500, stock_status: "In Stock" },
+      "L": { variant_id: 38185, price: 3500, stock_status: "In Stock" },
+      "XL": { variant_id: 38199, price: 3500, stock_status: "In Stock" },
+      "2XL": { variant_id: 38213, price: 3500, stock_status: "In Stock" },
+      "3XL": { variant_id: 42114, price: 3750, stock_status: "In Stock" },
+      "4XL": { variant_id: 66200, price: 3750, stock_status: "In Stock" }
     },
     "Sand": {
-      "S": { variant_id: 67436, price: 1722, stock_status: "In Stock" },
-      "M": { variant_id: 67441, price: 1722, stock_status: "In Stock" },
-      "L": { variant_id: 67446, price: 1722, stock_status: "In Stock" },
-      "XL": { variant_id: 67451, price: 1722, stock_status: "In Stock" },
-      "2XL": { variant_id: 67456, price: 1952, stock_status: "In Stock" },
-      "3XL": { variant_id: 67461, price: 2145, stock_status: "In Stock" }
+      "S": { variant_id: 67436, price: 3500, stock_status: "In Stock" },
+      "M": { variant_id: 67441, price: 3500, stock_status: "In Stock" },
+      "L": { variant_id: 67446, price: 3500, stock_status: "In Stock" },
+      "XL": { variant_id: 67451, price: 3500, stock_status: "In Stock" },
+      "2XL": { variant_id: 67456, price: 3500, stock_status: "In Stock" },
+      "3XL": { variant_id: 67461, price: 3750, stock_status: "In Stock" }
     },
     "White": {
-      "XS": { variant_id: 67829, price: 1722, stock_status: "In Stock" },
-      "S": { variant_id: 38163, price: 1722, stock_status: "In Stock" },
-      "M": { variant_id: 38177, price: 1722, stock_status: "In Stock" },
-      "L": { variant_id: 38191, price: 1722, stock_status: "In Stock" },
-      "XL": { variant_id: 38205, price: 1722, stock_status: "In Stock" },
-      "2XL": { variant_id: 38219, price: 1952, stock_status: "In Stock" },
-      "3XL": { variant_id: 42120, price: 2145, stock_status: "In Stock" },
-      "4XL": { variant_id: 66211, price: 2367, stock_status: "In Stock" }
+      "XS": { variant_id: 67829, price: 3500, stock_status: "In Stock" },
+      "S": { variant_id: 38163, price: 3500, stock_status: "In Stock" },
+      "M": { variant_id: 38177, price: 3500, stock_status: "In Stock" },
+      "L": { variant_id: 38191, price: 3500, stock_status: "In Stock" },
+      "XL": { variant_id: 38205, price: 3500, stock_status: "In Stock" },
+      "2XL": { variant_id: 38219, price: 3500, stock_status: "In Stock" },
+      "3XL": { variant_id: 42120, price: 3750, stock_status: "In Stock" },
+      "4XL": { variant_id: 66211, price: 3750, stock_status: "In Stock" }
     }
   },
   "6814c6d00ed813d9e5087aea": { // Black logo (no Black shirt)
