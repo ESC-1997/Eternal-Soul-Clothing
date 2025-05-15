a3ef4d694e0dd5d14cbf0805b5d4c28d8b547cf6
ESC-1997 <info@eternalsoul.co>
2025-05-15 08:48:25 -0500

    Create eternalSlashLogoColors.ts

---
+++ app/components/eternalSlashLogoColors.ts
@@ -0,0 +1,8 @@
+export const ES_PRODUCT_ID_TO_LOGO_COLOR: Record<string, string> = {
+  '6820b02093284a99660b189d': 'midnight_indigo',
+  '6820aed333803c3c4502120d': 'red',
+  '6820ad40471efa6af008268a': 'white',
+  '6820abb0471efa6af0082632': 'grey',
+  '681fe48893284a99660af2f9': 'black',
+  '681fe068ebfdaacb650ca1d7': 'violet'
+}; 
