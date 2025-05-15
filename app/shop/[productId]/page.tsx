78ea212926cd5048b3f520175c0405dcec3baae4
ESC-1997 <info@eternalsoul.co>
2025-05-15 12:53:14 -0500

    Update page.tsx

--- app/shop/[productId]/page.tsx
+++ app/shop/[productId]/page.tsx
@@ -21,6 +21,39 @@ interface Products {
 
 // This would typically come from your database or API
 const products: Products = {
+  'eternal-lotus-(black-&-grey)': {
+    id: '681449c6b03bb3ed0c01a685',
+    title: 'Eternal Lotus (Black & Grey)',
+    images: [
+      { src: '/images/eternal_lotus/eternal_lotus_black_front.jpg' },
+      { src: '/images/eternal_lotus/eternal_lotus_black_back.jpg' }
+    ],
+    variants: [
+      { id: '38164', title: 'Small', price: '4000' }
+    ]
+  },
+  'eternal-lotus---purple-floral-graphic-tee-': {
+    id: '681449c6b03bb3ed0c01a686',
+    title: 'Eternal Lotus - Purple Floral Graphic Tee',
+    images: [
+      { src: '/images/eternal_lotus/eternal_lotus_purple_front.jpg' },
+      { src: '/images/eternal_lotus/eternal_lotus_purple_back.jpg' }
+    ],
+    variants: [
+      { id: '38164', title: 'Small', price: '4000' }
+    ]
+  },
+  'vow-of-the-eternal': {
+    id: '681ac79a1207456e76092f24',
+    title: 'Vow of the Eternal',
+    images: [
+      { src: '/images/vow_of_the_eternal/black.jpg' },
+      { src: '/images/vow_of_the_eternal/black1.jpg' }
+    ],
+    variants: [
+      { id: '38164', title: 'Small', price: '4000' }
+    ]
+  },
   'eternal-awakening': {
     id: '681acbd3c9285dd17e0dd618',
     title: 'Eternal Awakening',
