fd53d03ba374fcc703d9f50df725d96eb6849fbc
ESC-1997 <info@eternalsoul.co>
2025-06-02 08:31:43 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,4 +11,13 @@ const nextConfig = {
   },
 }
 
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
 module.exports = nextConfig 
